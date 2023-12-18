717dbc60e6805dbc71a305c5e1150de7629caf13
keymoad <mohamadreza.najarbashii@gmail.com>
2023-12-18 22:02:26 +0330

    add hashing passwd to database

--- app/app/src/main/java/com/example/app/DatabaseHelper.java
+++ app/app/src/main/java/com/example/app/DatabaseHelper.java
@@ -5,15 +5,19 @@ import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.Cursor;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "sma";
-    private static final int DATABASE_VERSION = 2;
+    private static final int DATABASE_VERSION = 1;
 
     // Table name and column names
     private static final String TABLE_NAME = "Users";
     private static final String COLUMN_USERNAME = "username";
     private static final String COLUMN_PASSWORD = "password";
+    private static final String COLUMN_SALT = "salt";
     private static final String COLUMN_HOST = "host";
     private static final String COLUMN_PASSPHRASE = "passphrase";
 
@@ -21,6 +25,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " (" +
             COLUMN_USERNAME + " TEXT PRIMARY KEY," +
             COLUMN_PASSWORD + " TEXT," +
+            COLUMN_SALT + " TEXT," +
             COLUMN_HOST + " TEXT," +
             COLUMN_PASSPHRASE + " TEXT )";
 
@@ -37,12 +42,51 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // Drop the existing table if it exists
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
-
         // Recreate the table by calling onCreate
         onCreate(db);
     }
 
-    // Method to add user to the database
+    // Method to hash the password using salt
+    private String hashPassword(String password, String salt) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            digest.update(salt.getBytes());
+            byte[] hash = digest.digest(password.getBytes());
+
+            // Convert byte array to a string representation
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // Method to generate a salt
+    private String generateSalt() {
+        SecureRandom random = new SecureRandom();
+        byte[] saltBytes = new byte[16];
+        random.nextBytes(saltBytes);
+        return bytesToHex(saltBytes);
+    }
+
+    // Helper method to convert byte array to hexadecimal string
+    private String bytesToHex(byte[] bytes) {
+        StringBuilder hexString = new StringBuilder(2 * bytes.length);
+        for (byte b : bytes) {
+            hexString.append(String.format("%02x", b));
+        }
+        return hexString.toString();
+    }
+
+    // Method to add user to the database with salted and hashed password
     public boolean addUser(String username, String password, String host, String passphrase) {
         // Check for empty or null inputs
         if (username == null || username.isEmpty() ||
@@ -52,15 +96,20 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             return false;
         }
 
+        // Generate a salt
+        String salt = generateSalt();
+
+        // Hash the password using salt
+        String hashedPassword = hashPassword(password, salt);
+
         SQLiteDatabase db = this.getWritableDatabase();
-        // Create a ContentValues object to insert data
         ContentValues values = new ContentValues();
         values.put(COLUMN_USERNAME, username);
-        values.put(COLUMN_PASSWORD, password);
+        values.put(COLUMN_PASSWORD, hashedPassword);
+        values.put(COLUMN_SALT, salt);
         values.put(COLUMN_HOST, host);
         values.put(COLUMN_PASSPHRASE, passphrase);
 
-        // Insert the values into the database
         long result = db.insert(TABLE_NAME, null, values);
 
         if (result != -1) {
@@ -71,14 +120,12 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             android.util.Log.d("DatabaseHelper", "User insertion failed");
         }
 
-        // Check if insertion was successful
         return result != -1;
     }
 
     // Method to check if a username exists in the database
     public boolean checkUserExistence(String username) {
         SQLiteDatabase db = this.getReadableDatabase();
-        // Query to check if the username exists in the database
         Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE " +
                 COLUMN_USERNAME + "=?", new String[]{username});
 
@@ -90,11 +137,37 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     // Method to check user credentials for login
     public boolean checkUserCredentials(String username, String password) {
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE " +
-                COLUMN_USERNAME + "=? AND " + COLUMN_PASSWORD + "=?", new String[]{username, password});
+        Cursor cursor = null;
 
-        int count = cursor.getCount();
-        cursor.close();
-        return count > 0;
+        try {
+            cursor = db.rawQuery("SELECT " + COLUMN_PASSWORD + ", " + COLUMN_SALT + " FROM " +
+                    TABLE_NAME + " WHERE " + COLUMN_USERNAME + "=?", new String[]{username});
+
+            if (cursor != null && cursor.moveToFirst()) {
+                int passwordIndex = cursor.getColumnIndex(COLUMN_PASSWORD);
+                int saltIndex = cursor.getColumnIndex(COLUMN_SALT);
+
+                if (passwordIndex >= 0 && saltIndex >= 0) {
+                    String hashedPasswordFromDb = cursor.getString(passwordIndex);
+                    String salt = cursor.getString(saltIndex);
+
+                    if (hashedPasswordFromDb != null && salt != null) {
+                        // Hash the input password with the retrieved salt
+                        String hashedInputPassword = hashPassword(password, salt);
+
+                        // Compare the stored hashed password with the hashed input password
+                        return hashedPasswordFromDb.equals(hashedInputPassword);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+
+        return false;
     }
 }
